dcafd0efc6e3a950dd6456c299a0a76695160282
Mariana Garman <garman.mariana@gmail.com>
2019-03-24 01:16:13 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,4 @@
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
 
 def hello_t(array)
   i = 0
@@ -12,8 +7,9 @@ def hello_t(array)
 end
 end
 
-["Tim", "Tom", "Jim"].each do |name|
+hello_t(["Tim", "Tom", "Jim"]).each do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
+end
 
